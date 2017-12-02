e544445e92c035786e0e883bd41b374f39285f44
nivred <emaildervin@gmail.com>
2017-12-02 16:44:57 -0600

    database with product display, buy, and availability queries

---
+++ bamazonCustomer.js
@@ -0,0 +1,70 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "pS159iS218",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    displayProducts();
+});
+
+function displayProducts() {
+    var query = "SELECT products.item_id, products.product_name, products.price FROM products";
+    connection.query(query, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log("Product ID: " + res[i].item_id + " || Product: " + res[i].product_name + " || Price: " + res[i].price);
+        }
+        buyProduct();
+    });
+}
+
+function buyProduct() {
+    inquirer.prompt([
+        {
+            name: "productID",
+            type: "input",
+            message: "Please enter Product ID Number of item you would like to purchase",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "quantity?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function(answer) {
+        console.log(answer.productID);
+        console.log(answer.quantity);
+        checkAvailability(answer.productID, answer.quantity);
+    });
+};
+
+function checkAvailability(prodID, quantity) {
+    var query = "SELECT products.stock_quantity FROM products WHERE ?";
+    connection.query(query, { item_id: prodID }, function(err, res) {
+        console.log(res[0].stock_quantity);
+        if ((res[0].stock_quantity) < quantity) {
+            console.log("The product you are searching for is not is not in stock");
+        } else {
+            console.log("sure " + ((res[0].stock_quantity) - quantity));
+        }
+    });
+};
---
+++ bamazonProducts.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+    item_id INT(10) NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT(10),
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('The Human Equation', 'Books', '79.99', '57');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('Sunglasses', 'Accessories', '59.99', '23');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('Symphonized NRG 3.0 Earbuds', 'Electronics', '49.99', '87');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('Essential Oils Pendant', 'Accessories', '34.99', '46');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('Fire TV and Alexa Voice Remote', 'Electronics', '69.99', '8');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('Fitness Tracker', 'Electronics', '49.99', '92');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('Slippers', 'Clothing', '7.99', '17');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('Poisonfeather', 'Books', '24.95', '7');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('The Devils Game', 'Books', '19.95', '33');
+INSERT INTO `bamazon`.`products` (`product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('Bath Robe', 'Clothing', '39.99', '74');
